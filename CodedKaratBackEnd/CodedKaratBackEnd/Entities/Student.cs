1c0c25334c629c70ec92981a158d7a8e53c46e20
djaldrin <djaldrin14@gmail.com>
2025-01-23 01:15:09 +0800

    Add files via upload

---
+++ CodedKaratBackEnd/CodedKaratBackEnd/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace CodedKaratBackEnd.Entities
+{
+    public class Student
+    {
+        [BsonId]
+        [BsonElement("_id"),BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("first_name"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string? FirstName { get; set; }
+        [BsonElement("last_name"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string? LastName { get; set; }
+        [BsonElement("email"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string? Email { get; set; }
+      
+    }
+}
